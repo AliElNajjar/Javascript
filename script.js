10472c4b2e4bce3646a6067c0846ff8b295bc856
maestro-007 <alinajjar.91@hotmail.com>
2020-05-10 20:08:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -118,15 +118,29 @@ c) correct answer (I would use a number for this)
         }
     }
     
-    Question.prototype.checkAnswer = function(ans){
-        if (ans === this.correct){
+    Question.prototype.checkAnswer = function(ans, callback){
+        var sc;
+        if (ans === this.correct)
+        {
             console.log('Correct!');
-            sc++;
+            sc = callback(true);
         }
-        else console.log('Wrong answer, please try again :)');
+        else{
+            console.log('Wrong Answer, please try again.');
+            sc = callback(false);
+        }
+
+        this.displayScore(sc);
     }
+
     
 
+    Question.prototype.displayScore = function(score){
+        console.log('Your current score is: ' + score);
+        console.log('------------------------------');
+    }
+    
+    
     var q1 = new Question('Which football team is the strongest in the world?',
                         ['Brasil', 'Argentina', 'Germany', 'France', 'Italy'],
                         2);
@@ -140,29 +154,40 @@ c) correct answer (I would use a number for this)
                         0);
     
     var questions = [q1, q2, q3];
-    
-    function score(correct){
-        var sc = 0;
-        if(correct)
 
+    function score (){
+            var sc = 0;
+            return function (correct){
+             if(correct){
+                sc++;
+             }
+             return sc;
+            }
     }
+    
 
-    function nextQuestion(){
+    var keepScore = score();
+    
+    function nextQuestion () {
         var n = Math.floor(Math.random()*questions.length);
     
         questions[n].displayQuestion();
-        
-        var answer = prompt('Please select the correct answer!');
-        if (answer !=='exit'){
-            questions[n].checkAnswer(parseInt(answer));
-        
+    
+        var answer = prompt('Please write the number of the correct answer.');
+    
+        if (answer !== 'exit'){
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+
             nextQuestion();
         }
-        
     }
 
     nextQuestion();
-    
+
+
 })();
+
+
+
 
 
